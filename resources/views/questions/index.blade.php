09ba9f76115e3dc61ae826c04e98e4aef88822eb
unknown <yasirnaseer.0@gmail.com>
2020-04-23 15:48:10 +0500

    showing all questions

---
+++ resources/views/questions/index.blade.php
@@ -0,0 +1,26 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-12">
+            <div class="card">
+                <div class="card-header">Dashboard</div>
+
+                <div class="card-body">
+                    @foreach($questions as $question) 
+                        <div class="media">
+                            <div class="media-body">
+                                <h3>{{ $question->title}}</h3>
+                                {{ \Illuminate\Support\Str::limit($question->body, 250, '...') }}                            </div>
+                        </div>
+                        <hr>
+                    @endforeach
+
+                    {{ $questions->links() }}
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
