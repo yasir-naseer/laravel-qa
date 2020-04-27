b03263973c2ac058fd2a851df4ccd5b072a3d971
unknown <yasirnaseer.0@gmail.com>
2020-04-27 18:29:32 +0500

    adding votes to questions and answers

--- resources/views/questions/show.blade.php
+++ resources/views/questions/show.blade.php
@@ -5,30 +5,46 @@
     <div class="row justify-content-center">
         <div class="col-md-12">
             <div class="card">
-                <div class="card-header">
-                    <div class="d-flex align-items-center">
-                        <h2>{{ $question->title}}</h2>
-                        <div class="ml-auto">
-                            <a href="{{ route('questions.index') }}" class="btn btn-outline-secondary">See All Questions</a>
+                <div class="card-body">
+                    <div class="card-title">
+                        <div class="d-flex align-items-center">
+                            <h2>{{ $question->title}}</h2>
+                            <div class="ml-auto">
+                                <a href="{{ route('questions.index') }}" class="btn btn-outline-secondary">See All Questions</a>
+                            </div>
                         </div>
                     </div>
-                
-                </div>
+                    <hr>
 
-                <div class="card-body">
-                    {{ $question->body }}
-                    <div class="float-right mt-3">
+                    <div class="media">
+                        <div class="d-flex flex-column votes-control">
+                            <a title="This Quesiton is useful" class="vote-up">
+                                <i class="fas fa-caret-up fa-3x"></i>
+                            </a>
+                            <span class="votes-count">1234</span>
+                            <a title="This Quesiton is not useful" class="vote-down off">
+                                <i class="fas fa-caret-down fa-3x"></i>
+                            </a>
+                            <a title="Click to mark this question as favorite (click again to undo)" class="favorite mt-2 favorited">
+                                <i class="fas fa-star fa-2x"></i>
+                                <span class="favorites-count">123</span>
+                            </a>
+                        </div>
+                        <div class="media-body">
+                                {{ $question->body }}
+                                <div class="float-right mt-3">
                                     <span class="text-muted">Answered {{ $question->create_date}}</span>
                                     <div class="media mt-2">
                                         <a href="{{ $question->user->url }}" class="pr-2">
                                             <img src="{{ $question->user->avatar }}" alt="no img" >
                                         </a>
-
                                         <div class="media-body ">
                                             <a href="{{ $question->user->url }}">{{ $question->user->name }}</a>
                                         </div>
                                     </div>
                                 </div>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
@@ -44,7 +60,19 @@
                     <hr>
                     @foreach($question->answers as $answer)
                         <div class="media">
-                            <div class="media-body">    
+                            <div class="d-flex flex-column votes-control">
+                                <a title="This answer is useful" class="vote-up">
+                                    <i class="fas fa-caret-up fa-3x"></i>
+                                </a>
+                                <span class="votes-count">1234</span>
+                                <a title="This answer is not useful" class="vote-down off">
+                                    <i class="fas fa-caret-down fa-3x"></i>
+                                </a>
+                                <a title="mark this answer as best" class="vote-accepted mt-2 favorited">
+                                    <i class="fas fa-check fa-2x"></i>
+                                </a>
+                            </div>  
+                            <div class="media-body">  
                                 {{ $answer->body}}
                                 <div class="float-right mt-3">
                                     <span class="text-muted">Answered {{ $answer->created_date}}</span>
