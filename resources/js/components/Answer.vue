a7b3836b26a79710950797feed25dff361cc55d6
unknown <yasirnaseer.0@gmail.com>
2020-05-05 20:44:16 +0500

    making toast

--- resources/js/components/Answer.vue
+++ resources/js/components/Answer.vue
@@ -30,21 +30,42 @@ export default {
             .then(res => {
                 this.editing = false;
                 this.body = res.data.body;
-                alert(res.data.message);
+                this.$toast.success(res.data.message, "Success", { timeout: 3000 });
+                
             })
             .catch(err => {
-                alert(err.response.data.message);
+                this.$toast.error(err.response.data.message, "Error", { timeout: 3000 });
             })   
         },
         destroy() {
-           if(confirm('Are you sure you wants to delete the question?')){
-                axios.delete(`/questions/${this.questionId}/answers/${this.answerId}`)
-                .then(res => {
-                    $(this.$el).fadeOut(500, function() {
-                        alert(res.data.message);
-                    });
-                })
-           }
+            this.$toast.question('Are you sure about that?', "Confirm", {
+            timeout: 20000,
+            close: false,
+            overlay: true,
+            displayMode: 'once',
+            id: 'question',
+            zindex: 999,
+            position: 'center',
+            buttons: [
+                ['<button><b>YES</b></button>',  (instance, toast) => {
+        
+                    instance.hide({ transitionOut: 'fadeOut' }, toast, 'button');
+                        axios.delete(`/questions/${this.questionId}/answers/${this.answerId}`)
+                        .then(res => {
+                            $(this.$el).fadeOut(500, () =>  {
+                                this.$toast.success(res.data.message, "Success", { timeout: 3000 });
+                            });
+                        })
+                   
+                }, true],
+                ['<button>NO</button>', function (instance, toast) {
+        
+                    instance.hide({ transitionOut: 'fadeOut' }, toast, 'button');
+        
+                }],
+            ]
+        });
+           
         }
     },
     computed: {
